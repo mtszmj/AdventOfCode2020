44ed6e075f62eeb5e9761e62f402c53bfc74b162
Mateusz <mateuszmaj@o2.pl>
2020-12-28 23:07:54 +0100

    Add project files.

---
+++ AdventOfCode2020/Day01Task1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AdventOfCode2020
+{
+    public class Day01Task1
+    {
+        public long MultiplyTwoEntriesThatSumTo2020(int[] input)
+        {
+            if (input is null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+
+            if(input.Length < 2)
+            {
+                throw new ArgumentException("Less than two arguments");
+            }
+
+            for(var i = 0; i < input.Length; i++)
+            {
+                var first = input[i];
+                for(var j = i+1; j < input.Length; j++)
+                {
+                    var second = input[j];
+
+                    if (first + second == 2020)
+                        return first * second;
+                }
+            }
+
+            throw new ArgumentException("No correct result found");
+        }
+
+    }
+}
---
+++ AdventOfCode2020/Day01Task2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    public class Day01Task2
+    {
+        public long MultiplyThreeEntriesThatSumTo2020(int[] input)
+        {
+            if (input is null)
+            {
+                throw new ArgumentNullException(nameof(input));
+            }
+
+            if (input.Length < 3)
+            {
+                throw new ArgumentException("Less than two arguments");
+            }
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                var first = input[i];
+                for (var j = i + 1; j < input.Length; j++)
+                {
+                    var second = input[j];
+                    var sumOfTwo = first + second;
+                    if (sumOfTwo > 2020)
+                        continue;
+                    
+                    for(var k = j + 1; k < input.Length; k++)
+                    {
+                        var third = input[k];
+                        if (sumOfTwo + third == 2020)
+                            return first * second * third;
+                    }
+                }
+            }
+
+            throw new ArgumentException("No correct result found");
+        }
+    }
+}
