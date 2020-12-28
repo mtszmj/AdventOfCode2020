f95794dfe46cd061d1df2e14f28388ef676b1058
Mateusz <mateuszmaj@o2.pl>
2020-12-29 00:27:11 +0100

    Day 3 Task 1

---
+++ AdventOfCode2020.Tests/Day03Task1Tests.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Tests
+{
+    [TestFixture]
+    public class Day03Task1Tests
+    {
+        [Test]
+        public void count_7_trees()
+        {
+            var input =
+@"..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#";
+
+            var result = new Day03Task1().Count(input, 3, 1);
+
+            result.Should().Be(7);
+        }
+
+        [Test]
+        public void count_200_trees()
+        {
+            var input = File.ReadAllText("Files\\Day03.txt");
+
+            var result = new Day03Task1().Count(input, 3, 1);
+
+            result.Should().Be(200);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day03Task1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    public class Day03Task1
+    {
+        public const char Tree = '#';
+
+        public int Count(string input, int right, int down)
+        {
+            var lines = input.Split(Environment.NewLine);
+
+            var tileWidth = lines[0].Length;
+            var tileHeight = lines.Length;
+
+            var trees = 0;
+            var currentWidth = 0;
+
+            var path = new StringBuilder();
+
+            for (var height = 0; height < tileHeight; height += down)
+            {
+                var currentField = lines[height][currentWidth % tileWidth];
+                var isTree = currentField == Tree;
+                trees += isTree ? 1 : 0;
+
+                currentWidth += right;
+            }
+
+            return trees;
+        }
+    }
+}
