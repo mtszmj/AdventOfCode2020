55ec4d9afbbd1a3e8ee51237fd4be5448a2a305c
Mateusz <mateuszmaj@o2.pl>
2020-12-28 23:50:07 +0100

    Day 2 Task 1

---
+++ AdventOfCode2020.Tests/Day02Task1Tests.cs
@@ -0,0 +1,131 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Tests
+{
+    [TestFixture]
+    public class Day02Task1Tests
+    {
+        Day02Task1 Solution => new Day02Task1();
+
+        [Test]
+        public void parse_password_with_policy_throws_argument_null_exception_when_null()
+        {
+            Action action = () => Solution.ParsePasswordWithPolicy(null);
+
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Test]
+        public void parse_password_with_policy_throws_argument_exception_when_incorrect_format()
+        {
+            Action action = () => Solution.ParsePasswordWithPolicy("1-3 asdadsa");
+
+            action.Should().Throw<ArgumentException>().WithMessage("Incorrect format");
+        }
+
+        [Test]
+        public void parse_password_with_policy_throws_argument_exception_when_only_one_value_in_policy()
+        {
+            Action action = () => Solution.ParsePasswordWithPolicy("1 a: asdadsa");
+
+            action.Should().Throw<ArgumentException>().WithMessage("Incorrect values format");
+        }
+
+        [Test]
+        public void parse_password_with_policy_throws_argument_exception_when_more_than_two_values_in_policy()
+        {
+            Action action = () => Solution.ParsePasswordWithPolicy("1-3-5 a: asdadsa");
+
+            action.Should().Throw<ArgumentException>().WithMessage("Incorrect values format");
+        }
+
+        [Test]
+        public void parse_password_with_policy_throws_argument_exception_when_cannot_parse_first_policy_value()
+        {
+            Action action = () => Solution.ParsePasswordWithPolicy("a-5 a: asdadsa");
+
+            action.Should().Throw<ArgumentException>().WithMessage("Incorrect values format");
+        }
+
+        [Test]
+        public void parse_password_with_policy_throws_argument_exception_when_cannot_parse_second_policy_value()
+        {
+            Action action = () => Solution.ParsePasswordWithPolicy("1-b a: asdadsa");
+
+            action.Should().Throw<ArgumentException>().WithMessage("Incorrect values format");
+        }
+
+        [Test]
+        public void parse_password_with_policy_returns_correct_policy()
+        {
+            var (policy, _) = Solution.ParsePasswordWithPolicy("1-3 a: babcde");
+
+            policy.MinimumOccurance.Should().Be(1);
+            policy.MaximumOccurance.Should().Be(3);
+            policy.Letter.Should().Be('a');
+        }
+
+        [Test]
+        public void parse_password_with_policy_returns_correct_password()
+        {
+            var (_, password) = Solution.ParsePasswordWithPolicy("1-3 a: babcde");
+
+            password.Should().Be("babcde");
+        }
+
+        [TestCase(1, 3, 'a', "abcde")]
+        [TestCase(2, 9, 'c', "ccccccccc")]
+        public void password_validation_returns_true(int min, int max, char letter, string password)
+        {
+            var policy = new Day02Task1.PasswordPolicy(min, max, letter);
+
+            var result = policy.IsValid(password);
+
+            result.Should().BeTrue();
+        }
+
+        [TestCase(1, 3, 'b', "cdefg")]
+        public void password_validation_returns_false(int min, int max, char letter, string password)
+        {
+            var policy = new Day02Task1.PasswordPolicy(min, max, letter);
+
+            var result = policy.IsValid(password);
+
+            result.Should().BeFalse();
+        }
+
+        [Test]
+        public void returns_2()
+        {
+            var input = new string[]
+            {
+                "1-3 a: abcde",
+                "1-3 b: cdefg",
+                "2-9 c: ccccccccc"
+            };
+
+            var result = Solution.ValidatePasswords(input);
+
+            result.Should().Be(2);
+        }
+
+        [Test]
+        public void returns_536()
+        {
+            var input = File.ReadAllLines("Files\\Day02.txt");
+
+            var result = Solution.ValidatePasswords(input);
+            Console.WriteLine(result);
+
+
+            result.Should().Be(536);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day02Task1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    public class Day02Task1
+    {
+        public const string Separator = " ";
+        public const string ValuesSeparator = "-";
+
+        public int ValidatePasswords(string[] passwordWithPolicies)
+        {
+            return passwordWithPolicies.Select(ParsePasswordWithPolicy)
+                                       .Count(x => x.policy.IsValid(x.password) == true);
+        }
+
+        public (PasswordPolicy policy, string password) ParsePasswordWithPolicy(string line)
+        {
+            if (line is null)
+                throw new ArgumentNullException();
+
+            var parts = line.Split(Separator);
+
+            if (parts.Length != 3)
+                throw new ArgumentException("Incorrect format");
+
+            var policyValues = parts[0].Split(ValuesSeparator);
+
+            if (policyValues.Length != 2
+                || !int.TryParse(policyValues[0], out var min)
+                || !int.TryParse(policyValues[1], out var max)
+                )
+            {
+                throw new ArgumentException("Incorrect values format");
+            }
+
+            var policy = new PasswordPolicy(min, max, parts[1][0]);
+            return (policy, parts[2]);
+        }
+
+        public class PasswordPolicy 
+        {
+            public PasswordPolicy(int minimumOccurance, int maximumOccurance, char letterToCheck)
+            {
+                if (minimumOccurance > maximumOccurance)
+                    throw new ArgumentException("Minimum occurance cannot be higher than maximum occurance");
+
+                MinimumOccurance = minimumOccurance;
+                MaximumOccurance = maximumOccurance;
+                Letter = letterToCheck;
+            }
+
+            public int MinimumOccurance { get; set; }
+            public int MaximumOccurance { get; set; }
+            public char Letter { get; set; }
+
+            public bool IsValid(string password)
+            {
+                var count = password.Count(x => x == Letter);
+                return count >= MinimumOccurance && count <= MaximumOccurance;
+            }
+        }
+    }
+}
