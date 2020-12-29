d0fb4d1232c6fec184f966b2a7b5d028dd6b7f91
Mateusz <mateuszmaj@o2.pl>
2020-12-29 16:17:28 +0100

    Day 4 Task 1

---
+++ AdventOfCode2020.Tests/Day04Task1Tests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Tests
+{
+    [TestFixture]
+    public class Day04Task1Tests
+    {
+        [Test]
+        public void count_2_valid_passports_with_optional_cid()
+        {
+            var input =
+@"ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in";
+
+            var result = new Day04Task1().CheckPassports(input, new Day04Task1.CountryIdOptionalValidator());
+
+            result.Should().Be(2);
+        }
+
+        [Test]
+        public void count_228_valid_passports_with_optional_cid()
+        {
+            var input = File.ReadAllText("Files\\Day04.txt");
+
+            var result = new Day04Task1().CheckPassports(input, new Day04Task1.CountryIdOptionalValidator());
+
+            result.Should().Be(228);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day04Task1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    public class Day04Task1
+    {
+        private static string PassportsSeparator = $"{Environment.NewLine}{Environment.NewLine}";
+        private static string[] PassportDataSeparator = new string[] { " ", Environment.NewLine };
+        private static string PassportDataValueSeparator = ":";
+
+        public int CheckPassports(string data, IPassportValidator validator)
+        {
+            var passports = SplitPassports(data);
+            return passports.Select(x => PassportData.ParsePassport(x).IsValid(validator))
+                            .Count(x => x == true);
+        }
+
+        public string[] SplitPassports(string data)
+        {
+            return data.Split(PassportsSeparator);
+        }
+
+        public class PassportData
+        {
+            private Dictionary<string, Action<string>> KeyValueFunctions;
+
+            public PassportData()
+            {
+                KeyValueFunctions = new Dictionary<string, Action<string>>
+                {
+
+                    ["byr"] = byr => BirthYear = byr,
+                    ["iyr"] = iyr => IssueYear = iyr,
+                    ["eyr"] = eyr => ExpirationYear = eyr,
+                    ["hgt"] = hgt => Height = hgt,
+                    ["hcl"] = hcl => HairColor = hcl,
+                    ["ecl"] = ecl => EyeColor = ecl,
+                    ["pid"] = pid => PassportId = pid,
+                    ["cid"] = cid => CountryId = cid
+                };
+            }
+
+            public string BirthYear { get; set; }
+            public string IssueYear { get; set; }
+            public string ExpirationYear { get; set; }
+            public string Height { get; set; }
+            public string HairColor { get; set; }
+            public string EyeColor { get; set; }
+            public string PassportId { get; set; }
+            public string CountryId { get; set; }
+
+            public bool IsValid(IPassportValidator validator)
+            {
+                return validator?.Validate(this) ?? false;
+            }
+
+            public static PassportData ParsePassport(string data)
+            {
+                var fields = data.Split(PassportDataSeparator, StringSplitOptions.RemoveEmptyEntries);
+
+                var passport = new PassportData();
+                foreach (var field in fields)
+                {
+                    var keyValue = field.Split(PassportDataValueSeparator);
+                    if (keyValue.Length != 2)
+                        continue;
+
+                    if (!passport.KeyValueFunctions.TryGetValue(keyValue[0].Trim(), out var action))
+                        continue;
+
+                    action?.Invoke(keyValue[1].Trim());
+                }
+
+                return passport;
+            }
+        }
+
+        public interface IPassportValidator
+        {
+            bool Validate(PassportData data);
+        }
+
+        public class CountryIdOptionalValidator : IPassportValidator
+        {
+            public bool Validate(PassportData data)
+            {
+                return !(
+                string.IsNullOrWhiteSpace(data.BirthYear)
+                || string.IsNullOrWhiteSpace(data.IssueYear)
+                || string.IsNullOrWhiteSpace(data.ExpirationYear)
+                || string.IsNullOrWhiteSpace(data.Height)
+                || string.IsNullOrWhiteSpace(data.HairColor)
+                || string.IsNullOrWhiteSpace(data.EyeColor)
+                || string.IsNullOrWhiteSpace(data.PassportId)
+                );
+            }
+        }
+
+
+    }
+}
