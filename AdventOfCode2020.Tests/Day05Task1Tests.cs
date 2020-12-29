72404d386d7eb4f3371b13f5949995bbf16364e8
Mateusz <mateuszmaj@o2.pl>
2020-12-29 21:08:25 +0100

    Day 5 Task 1

---
+++ AdventOfCode2020.Tests/Day05Task1Tests.cs
@@ -0,0 +1,77 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Tests
+{
+    [TestFixture]
+    public class Day05Task1Tests
+    {
+        [TestCase("FFFFFFF", 0)]
+        [TestCase("FBFBBFF", 44)]
+        [TestCase("BBBBBBB", 127)]
+        public void finds_row(string input, int expected)
+        {
+            var result = new Day05Task1().FindRow(input);
+
+            result.Should().Be(expected);
+        }
+
+        [TestCase("LLL",0)]
+        [TestCase("LLR",1)]
+        [TestCase("LRL",2)]
+        [TestCase("LRR",3)]
+        [TestCase("RLL",4)]
+        [TestCase("RLR",5)]
+        [TestCase("RRL",6)]
+        [TestCase("RRR",7)]
+        public void finds_column(string input, int expected)
+        {
+            var result = new Day05Task1().FindColumn(input);
+
+            result.Should().Be(expected);
+        }
+
+        [TestCase("FBFBBFFRLR", 357)]
+        [TestCase("BFFFBBFRRR", 567)]
+        [TestCase("FFFBBBFRRR", 119)]
+        [TestCase("BBFFBBFRLL", 820)]
+        public void returns_seat_id(string input, int expected)
+        {
+            var result = new Day05Task1().FindSeatId(input);
+
+            result.Should().Be(expected);
+        }
+
+        [Test]
+        public void returns_max_seat_id()
+        {
+            var input = new[]
+            {
+                "FBFBBFFRLR",
+                "BFFFBBFRRR",
+                "FFFBBBFRRR",
+                "BBFFBBFRLL"
+            };
+
+            var result = new Day05Task1().FindHighestSeatId(input);
+
+            result.Should().Be(820);
+        }
+
+        [Test]
+        public void returns_959()
+        {
+            var input = File.ReadAllLines("Files\\Day05.txt");
+
+            var result = new Day05Task1().FindHighestSeatId(input);
+
+            result.Should().Be(959);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day05Task1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    public class Day05Task1
+    {
+        public int NumberOfRows { get; set; } = 128;
+        public int NumberOfColumns { get; set; } = 8;
+        public int NumberOfRowsLetters = 7;
+        public int NumberOfColumnsLetters = 3;
+
+        public int FindHighestSeatId(string[] input)
+        {
+            return input.Max(x => FindSeatId(x));
+        }
+
+        public int FindSeatId(string input)
+        {
+            var row = FindRow(input.Substring(0, NumberOfRowsLetters));
+            var column = FindColumn(input.Substring(NumberOfRowsLetters));
+
+            return row * 8 + column;
+        }
+
+        public int FindRow(string input)
+        {
+            var low = 0;
+            var high = NumberOfRows - 1;
+            for(int i = 0; i < NumberOfRowsLetters; i++)
+            {
+                var next = (high - low) / 2 + (high - low) % 2 + low;
+                if (input[i] == 'B')
+                    low = next;
+                else
+                    high = next;
+            }
+            return low;
+        }
+
+        public int FindColumn(string input)
+        {
+            var low = 0;
+            var high = NumberOfColumns - 1;
+            for (int i = 0; i < NumberOfColumnsLetters; i++)
+            {
+                var next = (high - low) / 2 + (high - low) % 2 + low;
+                if (input[i] == 'R')
+                    low = next;
+                else
+                    high = next;
+            }
+            return low;
+        }
+    }
+}
