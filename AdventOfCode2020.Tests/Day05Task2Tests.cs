b9161146eaac15acbd81e7b0899981db42c3e83e
Mateusz <mateuszmaj@o2.pl>
2020-12-29 21:08:31 +0100

    Day 5 Task 2

---
+++ AdventOfCode2020.Tests/Day05Task2Tests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Tests
+{
+    [TestFixture]
+    public class Day05Task2Tests
+    {
+        [Test]
+        public void finds_seat_527()
+        {
+            var solver = new Day05Task2();
+            var input = File.ReadAllLines("Files\\Day05.txt");
+
+            var result = solver.FindSeatIdWithMinusOneAndPlusOnePresent(input);
+
+            result.Should().Be(527);
+        }
+
+    }
+
+}
---
+++ AdventOfCode2020/Day05Task2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    public class Day05Task2
+    {
+        Day05Task1 solver = new Day05Task1();
+
+        public int FindSeatIdWithMinusOneAndPlusOnePresent(string[] input)
+        {
+            var result = -1;
+            var seatIds = input.Select(x => solver.FindSeatId(x)).OrderBy(x => x).ToList();
+            Console.WriteLine(string.Join(",", seatIds));
+            //var previous_minus2 = seatIds[0];
+            var previous_minus1 = seatIds[1];
+            foreach (var id in seatIds.OrderBy(x => x))
+            {
+                //if (previous_minus2 + 1 == previous_minus1 && previous_minus1 + 1 == id)
+                if (previous_minus1 + 2 == id)
+                    return previous_minus1 + 1;
+                else
+                {
+                    //previous_minus2 = previous_minus1;
+                    previous_minus1 = id;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
