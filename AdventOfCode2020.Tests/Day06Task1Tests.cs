6c99c28dcdc791612c136117339e550ccefea054
Mateusz <mateuszmaj@o2.pl>
2020-12-29 21:25:03 +0100

    Day 6 Task 1

---
+++ AdventOfCode2020.Tests/Day06Task1Tests.cs
@@ -0,0 +1,87 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Tests
+{
+    [TestFixture]
+    public class Day06Task1Tests
+    {
+        Day06Task1 _solver => new Day06Task1();
+
+        [Test]
+        public void splits_to_groups()
+        {
+            var input =
+@"abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b";
+            var result = _solver.SplitToGroups(input);
+
+            result.Should().Contain(new[]
+            {
+                "abc",
+                @"a
+b
+c",
+                @"ab
+ac",
+                @"a
+a
+a
+a",
+                "b"
+            });
+        }
+
+        [Test]
+        public void sums_yeses_to_11()
+        {
+            var input =
+@"abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b";
+            var result = _solver.Solve(input);
+
+            result.Should().Be(11);
+        }
+        
+        [Test]
+        public void sums_yeses_to_6457()
+        {
+            var input = File.ReadAllText("Files\\Day06.txt");
+            var result = _solver.Solve(input);
+
+            result.Should().Be(6457);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day06Task1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    public class Day06Task1
+    {
+        private static string GroupSeparator = $"{Environment.NewLine}{Environment.NewLine}";
+
+        public int Solve(string input)
+        {
+            var groups = SplitToGroups(input);
+            return groups.Sum(x => CountYesesInGroup(x));
+        }
+
+        public string[] SplitToGroups(string input)
+        {
+            return input.Split(GroupSeparator, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public int CountYesesInGroup(string group)
+        {
+            var yeses = group.Where(x => char.IsLetter(x)).ToHashSet();
+            return yeses.Count();
+        }
+    }
+}
