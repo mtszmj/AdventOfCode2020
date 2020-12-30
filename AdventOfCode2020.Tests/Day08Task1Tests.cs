7bdb1ca2f0e3cc1d871fa6cfea48bffcb2c288f2
Mateusz <mateuszmaj@o2.pl>
2020-12-30 16:19:41 +0100

    Day 8 Task 1

---
+++ AdventOfCode2020.Tests/Day08Task1Tests.cs
@@ -0,0 +1,185 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Tests
+{
+    [TestFixture]
+    public class Day08Task1Tests
+    {
+        [Test]
+        public void handle_nop_operation_returns_the_same_accumulator()
+        {
+            var state = new Day08Task1.State(123, 10);
+            var nop = new Day08Task1.Nop();
+
+            var result = nop.Handle(state);
+
+            result.Accumulator.Should().Be(123);
+        }
+        
+        [Test]
+        public void handle_nop_operation_returns_next_line_of_1_more()
+        {
+            var state = new Day08Task1.State(123, 10);
+            var nop = new Day08Task1.Nop();
+
+            var result = nop.Handle(state);
+
+            result.NextLine.Should().Be(11);
+        }
+
+        [Test]
+        public void handle_nop_operation_sets_was_executed_flag()
+        {
+            var state = new Day08Task1.State(123, 10);
+            var nop = new Day08Task1.Nop();
+
+            nop.WasExecuted.Should().BeFalse();
+            var result = nop.Handle(state);
+
+            nop.WasExecuted.Should().BeTrue();
+        }
+
+        [TestCase(10)]
+        [TestCase(-10)]
+        [TestCase(0)]
+        public void handle_acc_operation_changes_accumulator_value(int value)
+        {
+            var startingAccumulatorValue = 123;
+            var state = new Day08Task1.State(startingAccumulatorValue, 10);
+            var acc = new Day08Task1.Acc(value);
+
+            var result = acc.Handle(state);
+
+            result.Accumulator.Should().Be(startingAccumulatorValue + value);
+        }
+
+        [Test]
+        public void handle_acc_operation_returns_next_line_of_1_more()
+        {
+            var state = new Day08Task1.State(123, 10);
+            var acc = new Day08Task1.Acc(124);
+
+            var result = acc.Handle(state);
+
+            result.NextLine.Should().Be(11);
+        }
+
+        [Test]
+        public void handle_acc_operation_sets_was_executed_flag()
+        {
+            var state = new Day08Task1.State(123, 10);
+            var acc = new Day08Task1.Acc(124);
+
+            acc.WasExecuted.Should().BeFalse();
+            var result = acc.Handle(state);
+
+            acc.WasExecuted.Should().BeTrue();
+        }
+
+        [Test]
+        public void handle_jmp_operation_returns_the_same_accumulator()
+        {
+            var startingAccumulatorValue = 123;
+            var state = new Day08Task1.State(startingAccumulatorValue, 10);
+            var jmp = new Day08Task1.Jmp(11);
+
+            var result = jmp.Handle(state);
+
+            result.Accumulator.Should().Be(startingAccumulatorValue);
+        }
+
+
+        [TestCase(10)]
+        [TestCase(-10)]
+        [TestCase(0)]
+        public void handle_jmp_operation_returns_next_line_with_offset(int offset)
+        {
+            var startingLineValue = 10;
+            var state = new Day08Task1.State(123, startingLineValue);
+            var jmp = new Day08Task1.Jmp(offset);
+
+            var result = jmp.Handle(state);
+
+            result.NextLine.Should().Be(startingLineValue + offset);
+        }
+
+        [Test]
+        public void handle_jmp_operation_sets_was_executed_flag()
+        {
+            var state = new Day08Task1.State(123, 10);
+            var jmp = new Day08Task1.Jmp(124);
+
+            jmp.WasExecuted.Should().BeFalse();
+            var result = jmp.Handle(state);
+
+            jmp.WasExecuted.Should().BeTrue();
+        }
+
+        [Test]
+        public void parses_input_to_list_of_operations()
+        {
+            var input =
+@"nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6";
+
+            var solver = new Day08Task1();
+            var result = solver.ParseInput(input.Split(Environment.NewLine)).ToList();
+
+            result.Should().HaveCount(9);
+            result[0].Should().BeOfType<Day08Task1.Nop>();
+            result[1].Should().BeOfType<Day08Task1.Acc>().And.Subject.As<Day08Task1.Acc>().Value.Should().Be(1);
+            result[2].Should().BeOfType<Day08Task1.Jmp>().And.Subject.As<Day08Task1.Jmp>().Offset.Should().Be(4);
+            result[3].Should().BeOfType<Day08Task1.Acc>().And.Subject.As<Day08Task1.Acc>().Value.Should().Be(3);
+            result[4].Should().BeOfType<Day08Task1.Jmp>().And.Subject.As<Day08Task1.Jmp>().Offset.Should().Be(-3);
+            result[5].Should().BeOfType<Day08Task1.Acc>().And.Subject.As<Day08Task1.Acc>().Value.Should().Be(-99);
+            result[6].Should().BeOfType<Day08Task1.Acc>().And.Subject.As<Day08Task1.Acc>().Value.Should().Be(1);
+            result[7].Should().BeOfType<Day08Task1.Jmp>().And.Subject.As<Day08Task1.Jmp>().Offset.Should().Be(-4);
+            result[8].Should().BeOfType<Day08Task1.Acc>().And.Subject.As<Day08Task1.Acc>().Value.Should().Be(6);
+        }
+
+        [Test]
+        public void returns_5_acc_value_when_infite_loop_is_found()
+        {
+            var input =
+@"nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6";
+
+            var solver = new Day08Task1();
+            var result = solver.Solve(input.Split(Environment.NewLine));
+
+            result.Should().Be(5);
+        }
+
+        [Test]
+        public void returns_1867_acc_value_when_infite_loop_is_found()
+        {
+            var input = File.ReadAllText("Files\\Day08.txt");
+
+            var solver = new Day08Task1();
+            var result = solver.Solve(input.Split(Environment.NewLine));
+
+            result.Should().Be(1867);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day08Task1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    public class Day08Task1
+    {
+        public int Solve(string[] input)
+        {
+            var state = new State(0, 0);
+            var operations = ParseInput(input).ToList();
+
+            var nextOp = operations[state.NextLine];
+            while(!nextOp.WasExecuted)
+            {
+                state = nextOp.Handle(state);
+                nextOp = operations[state.NextLine];
+            }
+
+            return state.Accumulator;
+        }
+
+        public IEnumerable<Operation> ParseInput(string[] input)
+        {
+            foreach(var line in input.Where(x => !string.IsNullOrWhiteSpace(x)))
+            {
+                var lineParts = line.Trim().Split(" ");
+                var value = int.Parse(lineParts[1]);
+                yield return lineParts[0] switch
+                {
+                    "nop" => new Nop(),
+                    "acc" => new Acc(value),
+                    "jmp" => new Jmp(value),
+                    _ => throw new ArgumentOutOfRangeException(paramName: null, message: $"Out of range: {lineParts[0]}")
+                };
+            }
+        }
+
+        public class State
+        {
+            public State(int acc, int nextLine)
+            {
+                Accumulator = acc;
+                NextLine = nextLine;
+            }
+
+            public int Accumulator { get; }
+            public int NextLine { get; }
+        }
+
+        public abstract class Operation
+        {
+            public bool WasExecuted { get; private set; }
+            public State Handle(State state)
+            {
+                WasExecuted = true;
+                return HandleStateChange(state);
+            }
+
+            protected abstract State HandleStateChange(State state);
+        }
+
+        public class Nop : Operation
+        {
+            protected override State HandleStateChange(State state)
+            {
+                return new State(state.Accumulator, state.NextLine + 1);
+            }
+        }
+
+        public class Acc : Operation
+        {
+            public Acc(int value)
+            {
+                Value = value;
+            }
+
+            public int Value { get; }
+
+            protected override State HandleStateChange(State state)
+            {
+                return new State(state.Accumulator + Value, state.NextLine + 1);
+            }
+        }
+
+        public class Jmp : Operation
+        {
+            public Jmp(int offset)
+            {
+                Offset = offset;
+            }
+
+            public int Offset { get; }
+
+            protected override State HandleStateChange(State state)
+            {
+                return new State(state.Accumulator, state.NextLine + Offset);
+            }
+        }
+    }
+}
