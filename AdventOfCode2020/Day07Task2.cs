196469b60e51b571e2d9c9b55a39c290dd652bff
Mateusz <mateuszmaj@o2.pl>
2020-12-30 01:10:50 +0100

    Day 7 Task 2

---
+++ AdventOfCode2020/Day07Task2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    public class Day07Task2
+    {
+        Day07Task1 solver = new Day07Task1();
+
+        public int Solve(string input, string forColor)
+        {
+            var rules = solver.ParseRules(input).ToDictionary(x => x.Color, x => x);
+
+            List<(Day07Task1.Rule Rule, int Multiplier)> Nodes = new List<(Day07Task1.Rule Rule, int Multiplier)>
+            {
+                (rules[forColor], 1)
+            };
+
+            var index = 0;
+
+            var sum = 0;
+            while (index < Nodes.Count)
+            {
+                var node = Nodes[index];
+                foreach (var rule in node.Rule.Hold)
+                {
+                    Nodes.Add((rules[rule.Key], node.Multiplier * rule.Value));
+                    sum += node.Multiplier * rule.Value;
+                }
+                index++;
+            }
+
+            return sum;
+        }
+
+    }
+}
