67a11a11a5c37ea16d6d05e3fe09c537f2cde96b
Mateusz <mateuszmaj@o2.pl>
2021-01-01 20:12:05 +0100

    Day 9 Task 1

---
+++ AdventOfCode2020.Tests/Day09Task1Tests.cs
@@ -0,0 +1,102 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Tests
+{
+    [TestFixture]
+    public class Day09Task1Tests
+    {
+        Day09Task1 solver = new Day09Task1();
+        long[] _window = new[] { 1L, 3, 5, 7, 10, 21, 13, 21 };
+
+        [TestCase(4)]
+        [TestCase(6)]
+        [TestCase(8)]
+        [TestCase(10)]
+        [TestCase(11)]
+        [TestCase(14)]
+        [TestCase(22)]
+        [TestCase(13)]
+        [TestCase(16)]
+        [TestCase(24)]
+        [TestCase(12)]
+        [TestCase(15)]
+        [TestCase(18)]
+        [TestCase(26)]
+        [TestCase(17)]
+        [TestCase(20)]
+        [TestCase(28)]
+        [TestCase(23)]
+        [TestCase(31)]
+        [TestCase(34)]
+        public void sum_of_two_different_element_exist(int value)
+        {
+            var result = solver.DoesSumExist(value, _window);
+
+            result.Should().BeTrue();
+        }
+
+        [TestCase(5)]
+        [TestCase(7)]
+        [TestCase(9)]
+        [TestCase(19)]
+        [TestCase(21)]
+        [TestCase(25)]
+        [TestCase(27)]
+        [TestCase(29)]
+        [TestCase(30)]
+        [TestCase(32)]
+        [TestCase(33)]
+        [TestCase(42)]
+        public void sum_of_two_different_element_does_not_exist(int value)
+        {
+            var result = solver.DoesSumExist(value, _window);
+
+            result.Should().BeFalse();
+        }
+
+        [Test]
+        public void solves_127()
+        {
+            var input =
+@"35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576";
+            var result = solver.Solve(input, 5);
+
+            result.Should().Be(127);
+        }
+
+        [Test]
+        public void solves_257342611()
+        {
+            var input = File.ReadAllText("Files\\Day09.txt");
+            var result = solver.Solve(input, 25);
+
+            result.Should().Be(257342611L);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day09Task1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    public class Day09Task1
+    {
+        public long Solve(string numbersInput, int preambleLength)
+        {
+            long[] numbers = numbersInput.Split(Environment.NewLine).Select(x => long.Parse(x)).ToArray();
+            long[] window = numbers.Take(preambleLength).ToArray();
+            int eldestWindowIndex = 0;
+            int inputIndex = preambleLength;
+
+            for(int i = inputIndex; i < numbers.Length; i++)
+            {
+                long value = numbers[i];
+                if (!DoesSumExist(value, window))
+                    return value;
+
+                window[eldestWindowIndex] = value;
+                eldestWindowIndex = (eldestWindowIndex + 1) % preambleLength;
+            }
+
+            throw new InvalidOperationException("Solution not found");
+        }
+
+        public bool DoesSumExist(long value, long[] window)
+        {
+            var array = window.OrderBy(x => x).ToArray();
+
+            for(int left = 0; left < array.Length - 1; left++)
+            {
+                if(array[left] > value)
+                    continue;
+                
+                for(int right = array.Length - 1; right > left; right--)
+                {
+                    long lValue = array[left];
+                    long rValue = array[right];
+                    if (lValue != rValue && lValue + rValue == value)
+                        return true;
+
+                    if (lValue + rValue < value)
+                        break;
+                }
+            }
+
+            return false;
+        }
+    }
+}
