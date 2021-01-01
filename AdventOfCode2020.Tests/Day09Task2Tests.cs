424fb74b8e1cbb728765715ca250bce929c7b483
Mateusz <mateuszmaj@o2.pl>
2021-01-01 21:11:38 +0100

    Day 9 Task 2

---
+++ AdventOfCode2020.Tests/Day09Task2Tests.cs
@@ -0,0 +1,79 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Tests
+{
+    [TestFixture]
+    public class Day09Task2Tests
+    {
+        Day09Task2 _solver = new Day09Task2();
+
+        [Test]
+        public void finds_set_summing_to_127()
+        {
+            var input = new long[]
+            { 35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576 };
+
+            var result = _solver.FindSetWithSumOfGivenValue(127, input);
+            result.left.Should().Be(2);
+            result.right.Should().Be(5);
+        }
+
+
+        [Test]
+        public void sums_min_and_max_of_set()
+        {
+            var input = new long[]
+            { 35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576 };
+
+            var result = _solver.SumMinAndMax(2, 5, input);
+
+            result.Should().Be(62);
+        }
+
+        [Test]
+        public void solves_with_62()
+        {
+            var input =
+@"35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576";
+
+            var result = _solver.Solve(input, 5);
+
+            result.Should().Be(62);
+        }
+        [Test]
+        public void solves_with_N()
+        {
+            var input = File.ReadAllText("Files\\Day09.txt");
+
+            var result = _solver.Solve(input, 25);
+
+            result.Should().Be(35602097);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day09Task2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    public class Day09Task2
+    {
+        Day09Task1 task1 = new Day09Task1();
+
+        public long Solve(string numbersInput, int preambleLength)
+        {
+            var nonExistingSumOfTwoElements = task1.Solve(numbersInput, preambleLength);
+
+            long[] numbers = numbersInput.Split(Environment.NewLine).Select(x => long.Parse(x)).ToArray();
+            var (left, right) = FindSetWithSumOfGivenValue(nonExistingSumOfTwoElements, numbers);
+            var sum = SumMinAndMax(left, right, numbers);
+            return sum;
+        }
+
+        public (int left, int right) FindSetWithSumOfGivenValue(long nonExistingSumOfTwoElements, long[] numbers)
+        {
+            var setLeftIndex = 0;
+            var setRightIndex = 1;
+            var sum = numbers[setLeftIndex] + numbers[setRightIndex];
+            while (true)
+            {
+                if (sum == nonExistingSumOfTwoElements)
+                    return (setLeftIndex, setRightIndex);
+
+                if (setRightIndex == setLeftIndex + 1)
+                    sum += numbers[++setRightIndex];
+                else if (sum < nonExistingSumOfTwoElements)
+                    sum += numbers[++setRightIndex];
+                else sum -= numbers[setLeftIndex++];
+            }
+
+            throw new InvalidOperationException("Solution not found");
+        }
+
+        public long SumMinAndMax(int leftIndex, int rightIndex, long[] numbers)
+        {
+            var min = numbers.Skip(leftIndex).Take(rightIndex - leftIndex + 1).Min();
+            var max = numbers.Skip(leftIndex).Take(rightIndex - leftIndex + 1).Max();
+            return min + max;
+        }
+    }
+}
