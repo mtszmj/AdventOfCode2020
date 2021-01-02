5e1d713e41dfd57abcdab407bbade9976a1cf5d1
Mateusz <mateuszmaj@o2.pl>
2021-01-02 19:22:15 +0100

    Day 11 Task 1

---
+++ AdventOfCode2020.Tests/Day11Task1Tests.cs
@@ -0,0 +1,270 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AdventOfCode2020.Day11Task1;
+
+namespace AdventOfCode2020.Tests
+{
+    [TestFixture]
+    public class Day11Task1Tests
+    {
+        Day11Task1 _solver = new Day11Task1();
+        public string NL = Environment.NewLine;
+
+        string input1 => @"L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL";
+
+        [Test]
+        public void parses_input_correctly()
+        {
+            var input = $"L.L{NL}#L.{NL}LLL";
+
+            var result = _solver.ParseInput(input);
+
+            result.Length.Should().Be(9);
+            result.GetLength(0).Should().Be(3);
+            result.GetLength(1).Should().Be(3);
+
+            result[0, 0].Row.Should().Be(0);
+            result[0, 0].Column.Should().Be(0);
+            result[0, 0].Occupation.Should().Be(Occupation.Empty);
+
+            result[0, 1].Row.Should().Be(0);
+            result[0, 1].Column.Should().Be(1);
+            result[0, 1].Occupation.Should().Be(Occupation.Floor);
+
+            result[1, 0].Row.Should().Be(1);
+            result[1, 0].Column.Should().Be(0);
+            result[1, 0].Occupation.Should().Be(Occupation.Occupied);
+        }
+
+        [Test]
+        public void checks_seats_as_correct()
+        {
+            var input = $"L.L{NL}#L.{NL}LLL";
+            var seats = _solver.ParseInput(input);
+            var result = _solver.CheckSeats(seats, input);
+
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void checks_seats_as_incorrect()
+        {
+            var input = $"L.L{NL}#L.{NL}LLL";
+            var seats = _solver.ParseInput(input);
+            var result = _solver.CheckSeats(seats, $"L.L{NL}#L.{NL}LL.");
+
+            result.Should().BeFalse();
+        }
+
+
+        [Test]
+        public void occupy_seats_once()
+        {
+            var expectedState = @"#.##.##.##
+#######.##
+#.#.#..#..
+####.##.##
+#.##.##.##
+#.#####.##
+..#.#.....
+##########
+#.######.#
+#.#####.##";
+
+            var seats = _solver.ParseInput(input1);
+
+            PrintSeats(seats);
+            var result = _solver.OccupySeats(seats);
+            PrintSeats(seats);
+
+            var isInExpectedState = _solver.CheckSeats(seats, expectedState);
+
+            result.Should().BeTrue();
+            isInExpectedState.Should().BeTrue();
+        }
+
+        [Test]
+        public void occupy_seats_twice()
+        {
+            var expectedState = @"#.LL.L#.##
+#LLLLLL.L#
+L.L.L..L..
+#LLL.LL.L#
+#.LL.LL.LL
+#.LLLL#.##
+..L.L.....
+#LLLLLLLL#
+#.LLLLLL.L
+#.#LLLL.##";
+
+            var seats = _solver.ParseInput(input1);
+
+            var result = _solver.OccupySeats(seats);
+            result = _solver.OccupySeats(seats);
+            PrintSeats(seats);
+
+            var isInExpectedState = _solver.CheckSeats(seats, expectedState);
+
+            result.Should().BeTrue();
+            isInExpectedState.Should().BeTrue();
+        }
+
+        [Test]
+        public void occupy_seats_three_times()
+        {
+            var expectedState = @"#.##.L#.##
+#L###LL.L#
+L.#.#..#..
+#L##.##.L#
+#.##.LL.LL
+#.###L#.##
+..#.#.....
+#L######L#
+#.LL###L.L
+#.#L###.##";
+
+            var seats = _solver.ParseInput(input1);
+
+            var result = _solver.OccupySeats(seats);
+            result = _solver.OccupySeats(seats);
+            result = _solver.OccupySeats(seats);
+            PrintSeats(seats);
+
+            var isInExpectedState = _solver.CheckSeats(seats, expectedState);
+
+            result.Should().BeTrue();
+            isInExpectedState.Should().BeTrue();
+        }
+
+        [Test]
+        public void occupy_seats_four_times()
+        {
+            var expectedState = @"#.#L.L#.##
+#LLL#LL.L#
+L.L.L..#..
+#LLL.##.L#
+#.LL.LL.LL
+#.LL#L#.##
+..L.L.....
+#L#LLLL#L#
+#.LLLLLL.L
+#.#L#L#.##";
+
+            var seats = _solver.ParseInput(input1);
+
+            var result = _solver.OccupySeats(seats);
+            result = _solver.OccupySeats(seats);
+            result = _solver.OccupySeats(seats);
+            result = _solver.OccupySeats(seats);
+            PrintSeats(seats);
+
+            var isInExpectedState = _solver.CheckSeats(seats, expectedState);
+
+            result.Should().BeTrue();
+            isInExpectedState.Should().BeTrue();
+        }
+        [Test]
+        public void occupy_seats_five_times()
+        {
+            var expectedState = @"#.#L.L#.##
+#LLL#LL.L#
+L.#.L..#..
+#L##.##.L#
+#.#L.LL.LL
+#.#L#L#.##
+..L.L.....
+#L#L##L#L#
+#.LLLLLL.L
+#.#L#L#.##";
+
+            var seats = _solver.ParseInput(input1);
+
+            var result = _solver.OccupySeats(seats);
+            result = _solver.OccupySeats(seats);
+            result = _solver.OccupySeats(seats);
+            result = _solver.OccupySeats(seats);
+            result = _solver.OccupySeats(seats);
+            PrintSeats(seats);
+
+            var isInExpectedState = _solver.CheckSeats(seats, expectedState);
+
+            result.Should().BeTrue();
+            isInExpectedState.Should().BeTrue();
+        }
+
+        [Test]
+        public void occupy_seats_stabilizes_at_sixth_time()
+        {
+            var expectedState = @"#.#L.L#.##
+#LLL#LL.L#
+L.#.L..#..
+#L##.##.L#
+#.#L.LL.LL
+#.#L#L#.##
+..L.L.....
+#L#L##L#L#
+#.LLLLLL.L
+#.#L#L#.##";
+
+            var seats = _solver.ParseInput(input1);
+
+            var result = _solver.OccupySeats(seats);
+            result = _solver.OccupySeats(seats);
+            result = _solver.OccupySeats(seats);
+            result = _solver.OccupySeats(seats);
+            result = _solver.OccupySeats(seats);
+            result = _solver.OccupySeats(seats);
+            PrintSeats(seats);
+
+            var isInExpectedState = _solver.CheckSeats(seats, expectedState);
+
+            result.Should().BeFalse();
+            isInExpectedState.Should().BeTrue();
+        }
+
+        private void PrintSeats(Seat[,] seats)
+        {
+            var sb = new StringBuilder();
+            for (var r = 0; r < seats.GetLength(0); r++)
+            {
+                for(var c = 0; c < seats.GetLength(1); c++)
+                {
+                    sb.Append(seats[r, c].ToString());
+                }
+                sb.AppendLine();
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+
+        [Test]
+        public void count_37_occupied_seats()
+        {
+            var occupied = _solver.Solve(input1);
+
+            occupied.Should().Be(37);
+        }
+        [Test]
+        public void count_2424_occupied_seats()
+        {
+            var occupied = _solver.Solve(File.ReadAllText("Files\\Day11.txt"));
+
+            occupied.Should().Be(2424);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day11Task1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    public class Day11Task1
+    {
+        public virtual int NumberOfOccupiedAdjacementSeatsToLeaveSeat() => 4;
+
+        public long Solve(string input)
+        {
+            var seats = ParseInput(input);
+            while (OccupySeats(seats))
+                ;
+            return CountOccupiedSeats(seats);
+        }
+
+        private static long CountOccupiedSeats(Seat[,] seats)
+        {
+            var occupied = 0L;
+            foreach (var seat in seats)
+            {
+                if (seat.Occupation == Occupation.Occupied)
+                    occupied++;
+            }
+
+            return occupied;
+        }
+
+        public Seat[,] ParseInput(string input)
+        {
+            var rows = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            var numberOfRows = rows.Length;
+            var numberOfColumns = rows[0].Length;
+
+            var seats = new Seat[numberOfRows, numberOfColumns];
+            for (var r = 0; r < numberOfRows; r++)
+            {
+                for (var c = 0; c < numberOfColumns; c++)
+                {
+                    seats[r, c] = new Seat
+                    {
+                        Row = r,
+                        Column = c,
+                        Occupation = rows[r][c] switch
+                        {
+                            'L' => Occupation.Empty,
+                            '#' => Occupation.Occupied,
+                            '.' => Occupation.Floor,
+                            _ => throw new ArgumentException("Invalid character in input")
+                        }
+                    };
+                }
+            }
+
+            return seats;
+        }
+
+        public bool OccupySeats(Seat[,] seats)
+        {
+            var occupationHasChanged = false;
+
+            var snapshot = new Seat[seats.GetLength(0), seats.GetLength(1)];
+            for (var r = 0; r < seats.GetLength(0); r++)
+            {
+                for (var c = 0; c < seats.GetLength(1); c++)
+                {
+                    snapshot[r, c] = seats[r, c].Copy();
+                }
+            }
+
+            for (var r = 0; r < seats.GetLength(0); r++)
+            {
+                for (var c = 0; c < seats.GetLength(1); c++)
+                {
+
+                    var adjacementSeatsStates = CheckAdjacementSeats(snapshot, r, c);
+                    if (snapshot[r, c].IsEmpty
+                        && !adjacementSeatsStates.TryGetValue(Occupation.Occupied, out int _)
+                        )
+                    {
+                        seats[r, c].Occupation = Occupation.Occupied;
+                        occupationHasChanged = true;
+                    }
+                    else if (snapshot[r, c].IsOccupied
+                        && adjacementSeatsStates.TryGetValue(Occupation.Occupied, out int occupied)
+                        && occupied >= NumberOfOccupiedAdjacementSeatsToLeaveSeat()
+                        )
+                    {
+                        seats[r, c].Occupation = Occupation.Empty;
+                        occupationHasChanged = true;
+                    }
+                }
+            }
+
+            return occupationHasChanged;
+        }
+
+        public virtual Dictionary<Occupation, int> CheckAdjacementSeats(Seat[,] seats, int row, int column)
+        {
+            var counter = new Dictionary<Occupation, int>();
+            for (var r = row - 1; r <= row + 1; r++)
+                for (var c = column - 1; c <= column + 1; c++)
+                {
+                    if (r < 0 || r >= seats.GetLength(0)
+                        || c < 0 || c >= seats.GetLength(1)
+                        || (r == row && c == column))
+                        continue;
+
+                    var occupation = seats[r, c].Occupation;
+                    if (counter.ContainsKey(occupation))
+                        counter[occupation]++;
+                    else counter[occupation] = 1;
+                }
+
+            return counter;
+        }
+
+        public bool CheckSeats(Seat[,] seats, string expected)
+        {
+            var rows = expected.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            for (var r = 0; r < seats.GetLength(0); r++)
+            {
+                for (var c = 0; c < seats.GetLength(1); c++)
+                {
+                    switch (seats[r, c].Occupation)
+                    {
+                        case Occupation.Floor:
+                            if (rows[r][c] != '.') return false;
+                            break;
+                        case Occupation.Empty:
+                            if (rows[r][c] != 'L') return false;
+                            break;
+                        case Occupation.Occupied:
+                            if (rows[r][c] != '#') return false;
+                            break;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public class Seat
+        {
+            public int Row { get; set; }
+            public int Column { get; set; }
+            public Occupation Occupation { get; set; }
+
+            public bool IsEmpty => Occupation == Occupation.Empty;
+            public bool IsFloor => Occupation == Occupation.Floor;
+            public bool IsOccupied => Occupation == Occupation.Occupied;
+
+            public override string ToString()
+            {
+                return Occupation switch
+                {
+                    Occupation.Floor => ".",
+                    Occupation.Empty => "L",
+                    Occupation.Occupied => "#",
+                };
+            }
+
+            public Seat Copy()
+            {
+                return new Seat
+                {
+                    Row = Row,
+                    Column = Column,
+                    Occupation = Occupation
+                };
+            }
+        }
+
+        public enum Occupation
+        {
+            Floor,
+            Empty,
+            Occupied
+        }
+    }
+}
