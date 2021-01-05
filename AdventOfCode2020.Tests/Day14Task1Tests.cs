0474d93efe5dde663f9766a6ab9b11f1f8cac10d
Mateusz <mateuszmaj@o2.pl>
2021-01-05 22:27:37 +0100

    Day 14 Task 1

---
+++ AdventOfCode2020.Tests/Day14Task1Tests.cs
@@ -0,0 +1,101 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static AdventOfCode2020.Day14Task1;
+
+namespace AdventOfCode2020.Tests
+{
+    [TestFixture]
+    public class Day14Task1Tests
+    {
+        Day14Task1 _solver = new Day14Task1();
+
+        [Test]
+        public void check_regex_for_mask()
+        {
+            var mask = "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X";
+
+            var result = Day14Task1.MaskRegex.Match(mask);
+
+            result.Success.Should().BeTrue();
+            result.Groups[1].Value.Should().Be("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X");
+        }
+
+        [Test]
+        public void check_regex_for_memory()
+        {
+            var memory = "mem[8] = 11";
+
+            var result = Day14Task1.MemoryRegex.Match(memory);
+
+            result.Success.Should().BeTrue();
+            result.Groups[1].Value.Should().Be("8");
+            result.Groups[2].Value.Should().Be("11");
+        }
+
+        [Test]
+        public void mask_value_11_to_73()
+        {
+            var mask = new MaskCommand("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X");
+            var result = mask.ProcessValue(11);
+            result.Should().Be(73);
+        }
+
+        [Test]
+        public void parses_input_to_commands()
+        {
+            var input = @"mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0";
+
+            var result = _solver.ParseInput(input).ToList();
+
+            result.Should().HaveCount(4);
+            
+            result[0].Should().BeOfType<MaskCommand>()
+                .And.Subject.As<MaskCommand>().Value.Should().Be("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X");
+
+            result[1].Should().BeOfType<MemoryCommand>();
+            result[1].As<MemoryCommand>().Address.Should().Be(8);
+            result[1].As<MemoryCommand>().Value.Should().Be(11);
+
+            result[2].Should().BeOfType<MemoryCommand>();
+            result[2].As<MemoryCommand>().Address.Should().Be(7);
+            result[2].As<MemoryCommand>().Value.Should().Be(101);
+
+            result[3].Should().BeOfType<MemoryCommand>();
+            result[3].As<MemoryCommand>().Address.Should().Be(8);
+            result[3].As<MemoryCommand>().Value.Should().Be(0);
+        }
+
+        [Test]
+        public void sums_memory_to_165()
+        {
+            var input = @"mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+mem[8] = 11
+mem[7] = 101
+mem[8] = 0";
+
+            var result = _solver.Solve(input);
+
+            result.Should().Be(165);
+        }
+
+        [Test]
+        public void sums_file_memory_to_17765746710228()
+        {
+            var input = File.ReadAllText("Files\\Day14.txt");
+
+            var result = _solver.Solve(input);
+
+            result.Should().Be(17765746710228);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day14Task1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    public class Day14Task1
+    {
+        public static readonly Regex MaskRegex = new Regex("^mask = ([0,1,X]{36})$", RegexOptions.Compiled);
+        public static readonly Regex MemoryRegex = new Regex(@"^mem\[(\d+)\] = (\d+)$", RegexOptions.Compiled);
+
+        public long Solve(string input)
+        {
+            var system = new System(ParseInput(input));
+            system.Run();
+            return system.MemorySum();
+        }
+
+        public IEnumerable<ICommand> ParseInput(string input)
+        {
+            foreach (var line in input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+            {
+                var result = MemoryRegex.Match(line);
+                if(result.Success)
+                {
+                    yield return new MemoryCommand(result.Groups[1].Value, result.Groups[2].Value);
+                    continue;
+                }
+                result = MaskRegex.Match(line);
+                if (result.Success)
+                {
+                    yield return new MaskCommand(result.Groups[1].Value);
+                    continue;
+                }
+            }
+        }
+
+        public interface ICommand 
+        {
+            public void Process(System system);        
+        }
+
+        public class MaskCommand : ICommand
+        {
+            public MaskCommand(string mask)
+            {
+                Value = mask;
+                Set = Convert.ToInt64(mask.Replace('X', '0'), 2);
+                Reset = Convert.ToInt64(mask.Replace('X', '1'), 2);
+            }
+
+            public string Value { get; }
+            public long Set { get; }
+            public long Reset { get; }
+
+            public void Process(System system)
+            {
+                system.Mask = this;
+            }
+
+            public long ProcessValue(long value)
+            {
+                return value & Reset | Set;
+            }
+        }
+
+        public class MemoryCommand : ICommand
+        {
+            public MemoryCommand(string address, string value)
+            {
+                Address = long.Parse(address);
+                Value = long.Parse(value);
+            }
+
+            public long Address { get; }
+            public long Value { get; }
+
+            public void Process(System system)
+            {
+                system.Memory[Address] = system.MaskValue(Value);
+            }
+        }
+
+        public class System
+        {
+            public System(IEnumerable<ICommand> commands)
+            {
+                Commands = commands;
+                Initialize();
+            }
+
+            public void Initialize()
+            {
+                Memory = new Dictionary<long, long>();
+            }
+
+            public MaskCommand Mask { get; set; }
+            public Dictionary<long, long> Memory { get; private set; }
+            public IEnumerable<ICommand> Commands { get; }
+
+            public long MaskValue(long value)
+            {
+                return Mask.ProcessValue(value);
+            }
+
+            public void Run()
+            {
+                foreach(var cmd in Commands)
+                {
+                    cmd.Process(this);
+                }
+            }
+
+            public long MemorySum()
+            {
+                return Memory.Values.Sum();
+            }
+        }
+    }
+}
