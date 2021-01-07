7d43832f75be5e1c6425d5cadd4c6cc1fb4dd2b6
Mateusz <mateuszmaj@o2.pl>
2021-01-07 20:37:15 +0100

    Day 15 Task 1

---
+++ AdventOfCode2020.Tests/Day15Task1Tests.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Tests
+{
+    [TestFixture]
+    public class Day15Task1Tests
+    {
+        Day15Task1 _solver = new Day15Task1();
+
+        public static IEnumerable Data()
+        {
+            yield return new TestCaseData(new int[] { 0, 3, 6 }, 0, 1);
+            yield return new TestCaseData(new int[] { 0, 3, 6 }, 3, 2);
+            yield return new TestCaseData(new int[] { 0, 3, 6 }, 6, 3);
+            yield return new TestCaseData(new int[] { 0, 3, 6 }, 0, 4);
+            yield return new TestCaseData(new int[] { 0, 3, 6 }, 3, 5);
+            yield return new TestCaseData(new int[] { 0, 3, 6 }, 3, 6);
+            yield return new TestCaseData(new int[] { 0, 3, 6 }, 1, 7);
+            yield return new TestCaseData(new int[] { 0, 3, 6 }, 0, 8);
+            yield return new TestCaseData(new int[] { 0, 3, 6 }, 4, 9);
+            yield return new TestCaseData(new int[] { 0, 3, 6 }, 0, 10);
+            yield return new TestCaseData(new int[] { 0, 3, 6 }, 436, 2020);
+            yield return new TestCaseData(new int[] { 1, 3, 2 }, 1, 2020);
+            yield return new TestCaseData(new int[] { 2, 1, 3 }, 10, 2020);
+            yield return new TestCaseData(new int[] { 1, 2, 3 }, 27, 2020);
+            yield return new TestCaseData(new int[] { 2, 3, 1 }, 78, 2020);
+            yield return new TestCaseData(new int[] { 3, 2, 1 }, 438, 2020);
+            yield return new TestCaseData(new int[] { 3, 1, 2 }, 1836, 2020);
+            yield return new TestCaseData(new int[] { 1, 20, 11, 6, 12, 0 }, 1085, 2020);
+        }
+
+        [TestCaseSource(nameof(Data))]
+        public void solves_input(int[] input, int expected, int turn)
+        {
+            var result = _solver.Solve(input, turn);
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day15Task1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    public class Day15Task1
+    {
+        public int Solve(int[] input, int turn)
+        {
+            var data = new Dictionary<int, (int prev_2, int prev_1)>();
+
+            var last = 0;
+            for(var i = 1; i <= input.Length && i <= turn; i++)
+            {
+                last = input[i-1];
+                data[last] = (0, i);
+            }
+
+            var next = 0;
+            for(var i = input.Length + 1; i <= turn; i++)
+            {
+                var wasUsed = data.TryGetValue(last, out var prevs);
+                if (wasUsed && prevs.prev_2 == 0 || !wasUsed)
+                    next = 0;
+                else
+                    next = prevs.prev_1 - prevs.prev_2;
+
+                if(!data.TryGetValue(next, out var nextPrevs))
+                    nextPrevs = (0, 0);
+                data[next] = (nextPrevs.prev_1, i);
+
+                last = next;    
+            }
+
+            return last;
+        }
+    }
+}
