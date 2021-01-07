c5a06e6e5051dc86e15dc95a177edd03d7543d98
Mateusz <mateuszmaj@o2.pl>
2021-01-07 21:46:11 +0100

    Day 16 Task 1

---
+++ AdventOfCode2020.Tests/Day16Task1Tests.cs
@@ -0,0 +1,133 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Tests
+{
+    [TestFixture]
+    public class Day16Task1Tests
+    {
+        Day16Task1 _solver = new Day16Task1();
+
+        [Test]
+        public void solves_input()
+        {
+            var result = _solver.Solve(File.ReadAllText("Files\\Day16.txt"));
+
+            result.Should().Be(23044);
+        }
+
+        [Test]
+        public void solves_example()
+        {
+            var input = @"class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12";
+
+            var result = _solver.Solve(input);
+
+            result.Should().Be(71);
+        }
+
+        [Test]
+        public void parse_all()
+        {
+            var input = @"class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12";
+
+            var result = _solver.Parse(input);
+
+            result.Rules.Should().HaveCount(3);
+            result.YourTicket.Should().NotBeNull();
+            result.OtherTickets.Should().HaveCount(4);
+        }
+
+
+        [Test]
+        public void parse_rules()
+        {
+            var input = @"class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+";
+
+            var result = _solver.ParseRules(input);
+
+            result.Should().SatisfyRespectively(
+                f =>
+                {
+                    f.Name.Should().Be("class");
+                    f.ValidNumbers.Should().Contain(new int[] { 1, 2, 3, 5, 6, 7 });
+                },
+                s =>
+                {
+                    s.Name.Should().Be("row");
+                    s.ValidNumbers.Should().Contain(new int[] { 6, 7, 8, 9, 10, 11,
+                        33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44 });
+                },
+                t =>
+                {
+                    t.Name.Should().Be("seat");
+                    t.ValidNumbers.Should().Contain(new int[] { 13,14,15,16,17,18,19,
+                        20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
+                        45,46,47,48,49,50
+                    });
+                }
+                );
+        }
+
+        [Test]
+        public void parse_your_ticket()
+        {
+            var input = @"your ticket:
+7,1,14";
+
+            var result = _solver.ParseYourTicket(input);
+
+            result.Numbers.Should().Contain(new[] { 7, 1, 14 });
+        }
+
+        [Test]
+        public void parse_other_tickets()
+        {
+            var input = @"nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12";
+
+            var result = _solver.ParseOtherTickets(input);
+
+            result.Should().SatisfyRespectively(
+                f => f.Numbers.Should().Contain(new int[] { 7, 3, 47 }),
+                s => s.Numbers.Should().Contain(new int[] { 40, 4, 50 }),
+                t => t.Numbers.Should().Contain(new int[] { 55, 2, 20 }),
+                fo => fo.Numbers.Should().Contain(new int[] { 38, 6, 12 })
+                );
+        }
+    }
+}
---
+++ AdventOfCode2020/Day16Task1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    public class Day16Task1
+    {
+        private static string NL = Environment.NewLine;
+        
+        public long Solve(string input)
+        {
+            var data = Parse(input);
+
+            var allValid = data.Rules.SelectMany(x => x.Value.ValidNumbers).ToHashSet();
+            var sum = 0;
+            foreach(var ticket in data.OtherTickets)
+            {
+                sum += ticket.Numbers.Where(x => !allValid.Contains(x)).Sum();
+            }
+            return sum;
+        }
+
+
+        public Data Parse(string input)
+        {
+            var parts = input.Split($"{NL}{NL}", StringSplitOptions.RemoveEmptyEntries);
+
+            return new Data(
+                ParseRules(parts[0]).ToDictionary(x => x.Name, x => x), 
+                ParseYourTicket(parts[1]), 
+                ParseOtherTickets(parts[2]).ToList()
+                );
+
+        }
+
+        public IEnumerable<Rule> ParseRules(string input)
+        {
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in lines)
+            {
+                var match = Regex.Match(line, @"^(?<name>.+): (?<val11>\d+)\-(?<val12>\d+) or (?<val21>\d+)-(?<val22>\d+)$");
+
+                var name = match.Groups["name"].Value;
+                var val11 = int.Parse(match.Groups["val11"].Value);
+                var val12 = int.Parse(match.Groups["val12"].Value);
+                var val21 = int.Parse(match.Groups["val21"].Value);
+                var val22 = int.Parse(match.Groups["val22"].Value);
+
+                var rule = new Rule()
+                {
+                    Name = name
+                };
+
+                foreach (var value in Enumerable.Range(val11, val12 - val11 + 1)
+                                                .Concat(Enumerable.Range(val21, val22 - val21 + 1)))
+                {
+                    rule.ValidNumbers.Add(value);
+                }
+
+                yield return rule;
+            }
+        }
+
+        public Ticket ParseYourTicket(string input)
+        {
+            var ticket = new Ticket();
+            foreach(var value in input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)[1]
+                .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => int.Parse(x)))
+            {
+                ticket.Numbers.Add(value);
+            }
+            return ticket;
+        }
+
+        public IEnumerable<Ticket> ParseOtherTickets(string input)
+        {
+            foreach (var line in input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Skip(1))
+            {
+                var ticket = new Ticket();
+                foreach (var value in line.Split(",", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(x => int.Parse(x)))
+                {
+                    ticket.Numbers.Add(value);
+                }
+
+                yield return ticket;
+            }
+        }
+
+
+        public class Rule
+        {
+            public string Name { get; set; }
+            public HashSet<int> ValidNumbers { get; } = new HashSet<int>();
+        }
+
+        public class Ticket
+        {
+            public HashSet<int> Numbers { get; } = new HashSet<int>();
+        }
+
+        public class Data
+        {
+            public Data(Dictionary<string, Rule> rules,
+                Ticket yourTicket,
+                List<Ticket> otherTickets
+                )
+            {
+                Rules = rules;
+                YourTicket = yourTicket;
+                OtherTickets = otherTickets;
+            }
+
+            public Dictionary<string, Rule> Rules { get; } 
+            public Ticket YourTicket { get; }
+            public List<Ticket> OtherTickets { get; } 
+        }
+    }
+}
