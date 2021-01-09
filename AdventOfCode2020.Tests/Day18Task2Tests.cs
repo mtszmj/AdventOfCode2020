b665cfdd1a49024b1b2867c375f2b0af002d2ac6
Mateusz <mateuszmaj@o2.pl>
2021-01-09 21:54:01 +0100

    Day 18 Task 2

---
+++ AdventOfCode2020.Tests/Day18Task2Tests.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Tests
+{
+    [TestFixture]
+    public class Day18Task2Tests
+    {
+        Day18Task2 Solver() => new Day18Task2();
+        
+        [TestCase("1 + 2 * 3 + 4 * 5 + 6", 231)]
+        [TestCase("1 + (2 * 3) + (4 * (5 + 6))", 51)]
+        [TestCase("2 * 3 + (4 * 5)", 46)]
+        [TestCase("5 + (8 * 3 + 9 + 3 * 4 * 3)", 1445)]
+        [TestCase("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 669060)]
+        [TestCase("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 23340)]
+        public void solves_example(string input, long expected)
+        {
+            var solver = Solver();
+
+            var result = solver.SolveOne(input);
+
+            result.Should().Be(expected);
+        }
+
+        [Test]
+        public void solves_sum_of_lines()
+        {
+            var input = @"1 + 2 * 3 + 4 * 5 + 6
+1 + (2 * 3) + (4 * (5 + 6))
+2 * 3 + (4 * 5)
+5 + (8 * 3 + 9 + 3 * 4 * 3)
+5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))
+((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2";
+            var solver = Solver();
+
+            var result = solver.Solve(input);
+
+            result.Should().Be(231 + 51 + 46 + 1445 + 669060 + 23340);
+        }
+
+        [Test]
+        public void solves_input_from_file()
+        {
+            var input = File.ReadAllText("Files\\Day18.txt");
+            var solver = Solver();
+
+            var result = solver.Solve(input);
+
+            result.Should().Be(1);
+        }
+
+    }
+}
---
+++ AdventOfCode2020/Day18Task2.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    public class Day18Task2
+    {
+        private static readonly char[] Digits = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        const char Space = ' ';
+        const char Multiplication = '*';
+        const char Addition = '+';
+        const char OpenBracket = '(';
+        const char CloseBracket = ')';
+
+        public long Solve(string input)
+        {
+            return input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => SolveOne(x)).Sum();
+        }
+
+        public long SolveOne(string input)
+        {
+            if (input.Last() != '\n' && input.Last() != Space)
+                input += Space;
+
+            var data = new Queue<Object>();
+            var currentValue = "";
+            var index = 0;
+            var count = input.Length;
+            var brackets = 0;
+            var insideBrackets = "";
+            foreach (var c in input)
+            {
+                index++;
+
+                if (c == OpenBracket)
+                {
+                    brackets++;
+                    insideBrackets += c;
+                }
+                else if (c == CloseBracket)
+                {
+                    insideBrackets += c;
+                    if (--brackets == 0)
+                    {
+                        var inner = SolveOne(insideBrackets.Substring(1, insideBrackets.Length - 2));
+                        data.Enqueue(new Value { Val = inner });
+                        insideBrackets = "";
+                    }
+                }
+                else if (brackets > 0)
+                {
+                    insideBrackets += c;
+                }
+                else if (Digits.Contains(c))
+                {
+                    currentValue += c;
+                }
+                else if ((c == Space) && currentValue != "")
+                {
+                    data.Enqueue(new Value { Val = Convert.ToInt64(currentValue) });
+                    currentValue = "";
+                }
+                else if (c == Addition)
+                {
+                    data.Enqueue(Add.Instance);
+                }
+                else if (c == Multiplication)
+                {
+                    data.Enqueue(Multiply.Instance);
+                }
+            }
+
+            // solve additions
+            var list = data.ToList();
+            var newList = new List<Object>();
+            
+            while(list.Any(x => x is Add))
+            {
+                for (var i = 0; i < list.Count; i++)
+                {
+                    if (i < list.Count - 2 && list[i] is Value left && list[i + 1] is Add add && list[i + 2] is Value right)
+                    {
+                        newList.Add(new Value { Val = add.Perform(left.Val, right.Val) });
+                        i += 2;
+                    }
+                    else
+                    {
+                        newList.Add(list[i]);
+                    }
+                }
+                list = newList;
+                newList = new List<Object>();
+            }
+
+            var result = ((Value)list[0]).Val;
+            IOperation operation = null;
+            foreach (var current in list.Skip(1))
+            {
+                if (current is IOperation oper)
+                    operation = oper;
+                else if (current is Value val)
+                    result = operation.Perform(result, val.Val);
+            }
+
+            return result;
+        }
+
+
+        public interface IOperation
+        {
+            long Perform(long left, long right);
+        }
+
+        public class Multiply : IOperation
+        {
+            public static readonly Multiply Instance = new Multiply();
+            public long Perform(long left, long right) => left * right;
+        }
+
+        public class Add : IOperation
+        {
+            public static readonly Add Instance = new Add();
+            public long Perform(long left, long right) => left + right;
+        }
+
+        public class Value
+        {
+            public long Val { get; set; }
+        }
+    }
+}
