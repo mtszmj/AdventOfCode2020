c16a76f01aa9162c3e3a4f9a750ccdf04cb3ff25
Mateusz <mateuszmaj@o2.pl>
2021-01-19 21:18:10 +0100

    Day 21 Task 1

---
+++ AdventOfCode2020.Tests/Day21Task1Tests.cs
@@ -0,0 +1,89 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Tests
+{
+    [TestFixture]
+    public class Day21Task1Tests
+    {
+        Day21Task1 Solver() => new Day21Task1();
+
+        [Test]
+        public void parses_input()
+        {
+            var input = @"mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+trh fvjkl sbzzf mxmxvkd (contains dairy)
+sqjhc fvjkl (contains soy)
+sqjhc mxmxvkd sbzzf (contains fish)";
+
+            var solver = Solver();
+            var (allergens, ingredientsCount) = solver.ParseInput(input);
+
+            allergens.Should().HaveCount(3);
+            allergens.Should().ContainKeys("dairy", "fish", "soy");
+            allergens["dairy"].IngredientsCount.Should().HaveCount(7);
+            allergens["dairy"].IngredientsCount.Should().ContainKeys("mxmxvkd", "kfcds", "sqjhc", "nhms", "trh", "fvjkl", "sbzzf");
+            allergens["dairy"].IngredientsCount["mxmxvkd"].Should().Be(2);
+            allergens["dairy"].IngredientsCount["kfcds"].Should().Be(1);
+            allergens["dairy"].IngredientsCount["sqjhc"].Should().Be(1);
+            allergens["dairy"].IngredientsCount["nhms"].Should().Be(1);
+            allergens["dairy"].IngredientsCount["trh"].Should().Be(1);
+            allergens["dairy"].IngredientsCount["fvjkl"].Should().Be(1);
+            allergens["dairy"].IngredientsCount["sbzzf"].Should().Be(1);
+            allergens["dairy"].Occurances.Should().Be(2);
+
+            allergens["fish"].IngredientsCount.Should().HaveCount(5);
+            allergens["fish"].IngredientsCount.Should().ContainKeys("mxmxvkd", "kfcds", "sqjhc", "nhms", "sbzzf");
+            allergens["fish"].IngredientsCount["mxmxvkd"].Should().Be(2);
+            allergens["fish"].IngredientsCount["kfcds"].Should().Be(1);
+            allergens["fish"].IngredientsCount["sqjhc"].Should().Be(2);
+            allergens["fish"].IngredientsCount["nhms"].Should().Be(1);
+            allergens["fish"].IngredientsCount["sbzzf"].Should().Be(1);
+            allergens["fish"].Occurances.Should().Be(2);
+
+            allergens["soy"].IngredientsCount.Should().HaveCount(2);
+            allergens["soy"].IngredientsCount.Should().ContainKeys("sqjhc", "fvjkl");
+            allergens["soy"].IngredientsCount["sqjhc"].Should().Be(1);
+            allergens["soy"].IngredientsCount["fvjkl"].Should().Be(1);
+            allergens["soy"].Occurances.Should().Be(1);
+
+            ingredientsCount["mxmxvkd"].Should().Be(3);
+            ingredientsCount["kfcds"].Should().Be(1);
+            ingredientsCount["sqjhc"].Should().Be(3);
+            ingredientsCount["nhms"].Should().Be(1);
+            ingredientsCount["trh"].Should().Be(1);
+            ingredientsCount["fvjkl"].Should().Be(2);
+            ingredientsCount["sbzzf"].Should().Be(2);
+        }
+
+        [Test]
+        public void solves_example()
+        {
+            var input = @"mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+trh fvjkl sbzzf mxmxvkd (contains dairy)
+sqjhc fvjkl (contains soy)
+sqjhc mxmxvkd sbzzf (contains fish)";
+
+            var solver = Solver();
+            var result = solver.Solve(input);
+
+            result.Should().Be(5);
+        }
+
+        [Test]
+        public void solves_input()
+        {
+            var input = File.ReadAllText("Files\\Day21.txt");
+            var solver = Solver();
+            var result = solver.Solve(input);
+
+            result.Should().Be(2282);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day21Task1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    public class Day21Task1
+    {
+        public (Dictionary<string, Allergen> Allergens, Dictionary<string, int> IngredientsCount) ParseInput(string input)
+        {
+            Dictionary<string, Allergen> allergens = new Dictionary<string, Allergen>();
+            Dictionary<string, int> ingredientsCount = new Dictionary<string, int>();
+
+            foreach(var line in input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+            {
+                var ingredientsAndAllergens = line.Split("(contains ");
+                var ingredients = ingredientsAndAllergens[0].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var aller = ingredientsAndAllergens[1].Replace(")", "").Split(", ");
+
+                foreach(var a in aller)
+                {
+                    if (allergens.ContainsKey(a))
+                        allergens[a].AddIngredients(ingredients);
+                    else
+                        allergens[a] = new Allergen(a, ingredients);
+                }
+
+                foreach(var i in ingredients)
+                {
+                    if (ingredientsCount.ContainsKey(i))
+                        ingredientsCount[i]++;
+                    else
+                        ingredientsCount[i] = 1;
+                }
+            }
+
+            return (allergens, ingredientsCount);
+        }
+
+        public class Allergen
+        {
+            public Allergen(string name, string[] ingredients)
+            {
+                Name = name;
+                AddIngredients(ingredients);
+            }
+
+            public string Name { get; set; }
+            public Dictionary<string, int> IngredientsCount { get; } = new Dictionary<string, int>();
+            public int Occurances { get; set; }
+            public string Ingredient { get; set; }
+
+            public void AddIngredients(string[] ingredients)
+            {
+                Occurances++;
+                foreach(var ing in ingredients)
+                {
+                    if (IngredientsCount.ContainsKey(ing))
+                        IngredientsCount[ing] += 1;
+                    else
+                        IngredientsCount[ing] = 1;
+                }
+            }
+        }
+
+        public int Solve(string input)
+        {
+            var (allergens, ingredientsCount) = ParseInput(input);
+            var copyOfAllergens = new Dictionary<string, Allergen>(allergens);
+            var assignedIngredients = new HashSet<string>();
+            while (copyOfAllergens.Any())
+            {
+                var copy = new Dictionary<string, Allergen>(copyOfAllergens);
+                foreach(var all in copy.Values.Where(x => x.Ingredient == null).ToArray())
+                {
+                    var countOfMaxOccurances = all.IngredientsCount.Count(x => x.Value == all.Occurances && !assignedIngredients.Contains(x.Key));
+                    if(countOfMaxOccurances == 1)
+                    {
+                        all.Ingredient = all.IngredientsCount.First(x => x.Value == all.Occurances && !assignedIngredients.Contains(x.Key)).Key;
+                        assignedIngredients.Add(all.Ingredient);
+                        copyOfAllergens.Remove(all.Name);
+                    }
+                }
+            }
+
+            return ingredientsCount.Where(x => !assignedIngredients.Contains(x.Key)).Sum(x => x.Value);
+        }
+    }
+}
