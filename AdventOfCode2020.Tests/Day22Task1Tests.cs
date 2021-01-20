ce2d2c3b0bd32c9d163e11ae17fb9a20c1c2f2ab
Mateusz <mateuszmaj@o2.pl>
2021-01-20 21:12:21 +0100

    Day 22 Task 1

---
+++ AdventOfCode2020.Tests/Day22Task1Tests.cs
@@ -0,0 +1,126 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020.Tests
+{
+    [TestFixture]
+    public class Day22Task1Tests
+    {
+        Day22Task1 Solver() => new Day22Task1();
+        string Example() => @"Player 1:
+9
+2
+6
+3
+1
+
+Player 2:
+5
+8
+4
+7
+10";
+        [Test]
+        public void solves_example()
+        {
+            var input = Example();
+            var solver = Solver();
+            
+            var result = solver.Solve(input);
+
+            result.Should().Be(306);
+        }
+
+        [Test]
+        public void parses_example()
+        {
+            var input = Example();
+            var solver = Solver();
+
+            var (p1, p2) = solver.Parse(input);
+
+            p1.Should().ContainInOrder(new[] { 9, 2, 6, 3, 1 });
+            p2.Should().ContainInOrder(new[] { 5, 8, 4, 7, 10 });
+        }
+
+
+        [Test]
+        public void player1_wins_first_round()
+        {
+            var input = Example();
+            var solver = Solver();
+
+            var (p1, p2) = solver.Parse(input);
+
+            int round = 0;
+            var winners = new List<int>();
+            solver.PlayOneRound(p1, p2, ref round, winners);
+
+            p1.Should().ContainInOrder(new[] { 2, 6, 3, 1, 9, 5 });
+            p2.Should().ContainInOrder(new[] { 8, 4, 7, 10 });
+            round.Should().Be(1);
+            winners.Should().ContainInOrder(new[] { 1 });
+        }
+
+        [Test]
+        public void player2_wins_second_round()
+        {
+            var input = Example();
+            var solver = Solver();
+
+            var (p1, p2) = solver.Parse(input);
+
+            int round = 0;
+            var winners = new List<int>();
+            solver.PlayOneRound(p1, p2, ref round, winners);
+            solver.PlayOneRound(p1, p2, ref round, winners);
+
+            p1.Should().ContainInOrder(new[] { 6, 3, 1, 9, 5 });
+            p2.Should().ContainInOrder(new[] { 4, 7, 10, 8, 2 });
+            round.Should().Be(2);
+            winners.Should().ContainInOrder(new[] { 1, 2 });
+        }
+
+        [Test]
+        public void player2_wins()
+        {
+            var input = Example();
+            var solver = Solver();
+
+            var (p1, p2) = solver.Parse(input);
+
+            solver.Play(p1, p2);
+
+            p1.Should().BeEmpty();
+            p2.Should().ContainInOrder(new[] { 3, 2, 10, 6, 8, 5, 9, 4, 7, 1 });
+        }
+
+        [Test]
+        public void calculates_result()
+        {
+            var player = new[] { 3, 2, 10, 6, 8, 5, 9, 4, 7, 1 };
+            var solver = Solver();
+
+            var result = solver.CalculateResult(player);
+
+            result.Should().Be(306);
+        }
+
+        [Test]
+        public void solves_input()
+        {
+            var input = File.ReadAllText("Files\\Day22.txt");
+            var solver = Solver();
+
+            var result = solver.Solve(input);
+
+            result.Should().Be(31269);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day22Task1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    public class Day22Task1
+    {
+        public long Solve(string input)
+        {
+            var (p1, p2) = Parse(input);
+            Play(p1, p2);
+
+
+            return p1.Count > p2.Count ? CalculateResult(p1) : CalculateResult(p2);
+        }
+
+        public long CalculateResult(IReadOnlyCollection<int> player)
+        {
+            var count = player.Count;
+            return player.Aggregate(0L, (result, current) => result += current * count--);
+        }
+
+        public void Play(Queue<int> p1, Queue<int> p2)
+        {
+            var round = 0;
+            var winners = new List<int>();
+            while (p1.Any() && p2.Any())
+            {
+                PlayOneRound(p1, p2, ref round, winners);
+            }
+        }
+
+        public void PlayOneRound(Queue<int> p1, Queue<int> p2, ref int round, List<int> winners)
+        {
+            round++;
+            var p1Card = p1.Dequeue();
+            var p2Card = p2.Dequeue();
+
+            if (p1Card > p2Card)
+            {
+                p1.Enqueue(p1Card);
+                p1.Enqueue(p2Card);
+                winners.Add(1);
+            }
+            else if (p1Card < p2Card)
+            {
+                p2.Enqueue(p2Card);
+                p2.Enqueue(p1Card);
+                winners.Add(2);
+            }
+            else
+            {
+                throw new ArgumentException($"Unexpected: Draw in round {round}!");
+            }
+        }
+
+        public (Queue<int> Player1, Queue<int> Player2) Parse(string input)
+        {
+            var players = input.Split($"{Environment.NewLine}{Environment.NewLine}", StringSplitOptions.RemoveEmptyEntries);
+
+            var player1 = new Queue<int>(players[0].Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(int.Parse));
+            var player2 = new Queue<int>(players[1].Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(int.Parse));
+
+            return (player1, player2);
+        }
+    }
+}
