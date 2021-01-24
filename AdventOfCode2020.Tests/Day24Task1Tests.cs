42cf7be20877074b308bb458967cc449e2dbd7e6
Mateusz <mateuszmaj@o2.pl>
2021-01-24 13:59:25 +0100

    Day 24 Task 1

---
+++ AdventOfCode2020.Tests/Day24Task1Tests.cs
@@ -0,0 +1,128 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AdventOfCode2020.Day24Task1;
+
+namespace AdventOfCode2020.Tests
+{
+    [TestFixture]
+    public class Day24Task1Tests
+    {
+        Day24Task1 Solver() => new Day24Task1();
+
+        [Test]
+        public void solves_input()
+        {
+            var input = File.ReadAllText("Files\\Day24.txt");
+
+            var solver = Solver();
+
+            var result = solver.Solve(input);
+
+            result.Should().Be(320);
+        }
+
+        [Test]
+        public void solves_example()
+        {
+            var input = @"sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew";
+
+            var solver = Solver();
+
+            var result = solver.Solve(input);
+
+            result.Should().Be(10);
+        }
+
+        [Test]
+        public void finds_coordinate()
+        {
+            var input = "sesenwnenenewseeswwswswwnenewsewsw";
+            var solver = Solver();
+
+            //se   se    nw  ne   ne   ne   w   se   e   sw   w   sw   sw    w    ne   ne   w   se    w     sw
+            //-1,1 -2,2 -1,1 0,2  1,3  2,4  2,2 1,3 1,5  0,4  0,2 -1,1 -2,0 -2-2  -1,-1 0,0 0-2 -1,-1 -1,-3 -2,-4
+            var result = solver.FindCoordinates(input);
+
+            result.X.Should().Be(-4);
+            result.Y.Should().Be(-2);
+        }
+
+        [TestCase("nenwswse")]
+        [TestCase("nenwsesw")]
+        [TestCase("seswnenw")]
+        [TestCase("swsenenw")]
+        [TestCase("ew")]
+        [TestCase("we")]
+        [TestCase("wnese")]
+        [TestCase("eswnw")]
+        public void moves_to_zero(string input)
+        {
+            var solver = Solver();
+
+            var result = solver.FindCoordinates(input);
+
+            result.X.Should().Be(0);
+            result.Y.Should().Be(0);
+        }
+
+        [Test]
+        public void parses_single_input()
+        {
+            var input = "sesenwnenenewseeswwswswwnenewsewsw";
+            var solver = Solver();
+
+            var result = solver.ParseInstruction(input);
+
+            //se se nw ne ne ne w se e sw w sw sw w ne ne w se w sw
+            result.Should().HaveCount(20);
+            result.Should().SatisfyRespectively(
+                r => r.Should().BeOfType<SouthEast>(),
+                r => r.Should().BeOfType<SouthEast>(),
+                r => r.Should().BeOfType<NorthWest>(),
+                r => r.Should().BeOfType<NorthEast>(),
+                r => r.Should().BeOfType<NorthEast>(),
+                r => r.Should().BeOfType<NorthEast>(),
+                r => r.Should().BeOfType<West>(),
+                r => r.Should().BeOfType<SouthEast>(),
+                r => r.Should().BeOfType<East>(),
+                r => r.Should().BeOfType<SouthWest>(),
+                r => r.Should().BeOfType<West>(),
+                r => r.Should().BeOfType<SouthWest>(),
+                r => r.Should().BeOfType<SouthWest>(),
+                r => r.Should().BeOfType<West>(),
+                r => r.Should().BeOfType<NorthEast>(),
+                r => r.Should().BeOfType<NorthEast>(),
+                r => r.Should().BeOfType<West>(),
+                r => r.Should().BeOfType<SouthEast>(),
+                r => r.Should().BeOfType<West>(),
+                r => r.Should().BeOfType<SouthWest>()
+                ) ;
+        }
+
+    }
+}
---
+++ AdventOfCode2020/Day24Task1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    public class Day24Task1
+    {
+
+        public int Solve(string input)
+        {
+            var dict = new Dictionary<(int X, int Y), bool>();
+            foreach(var line in input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+            {
+                var (x, y) = FindCoordinates(line);
+                if(dict.TryGetValue((x,y), out bool value))
+                {
+                    dict[(x, y)] = !value;
+                }
+                else
+                {
+                    dict[(x, y)] = true;
+                }
+            }
+
+            return dict.Values.Count(x => x == true);
+        }
+
+        public (int X, int Y) FindCoordinates(string line)
+        {
+            var instructions = ParseInstruction(line);
+
+            var x = 0;
+            var y = 0;
+            foreach(var i in instructions)
+            {
+                (x, y) = i.Move(x, y);
+            }
+
+            return (x, y);
+        }
+
+        public IEnumerable<IInstruction> ParseInstruction(string line)
+        {
+            string memory = null;
+            var index = 0;
+            while(index < line.Length)
+            {
+                if (line[index] == 's' || line[index] == 'n')
+                {
+                    memory = line.Substring(index, 2);
+                    index += 2;
+                }
+                else memory = line.Substring(index++, 1);
+
+                yield return memory switch
+                {
+                    "e" => new East(),
+                    "se" => new SouthEast(),
+                    "sw" => new SouthWest(),
+                    "w" => new West(),
+                    "nw" => new NorthWest(),
+                    "ne" => new NorthEast(),
+                    _ => throw new ArgumentException("incorrect value in memory")
+                };
+            }
+        }
+
+        public interface IInstruction
+        {
+            (int X, int Y) Move(int X, int Y);
+        }
+
+        public class East : IInstruction
+        {
+            public (int X, int Y) Move(int X, int Y)
+            {
+                return (X + 2, Y);
+            }
+        }
+
+        public class SouthEast : IInstruction
+        {
+            public (int X, int Y) Move(int X, int Y)
+            {
+                return (X + 1, Y - 1);
+            }
+        }
+
+        public class SouthWest : IInstruction
+        {
+            public (int X, int Y) Move(int X, int Y)
+            {
+                return (X - 1, Y - 1);
+            }
+        }
+
+        public class West : IInstruction
+        {
+            public (int X, int Y) Move(int X, int Y)
+            {
+                return (X - 2, Y);
+            }
+        }
+
+        public class NorthWest : IInstruction
+        {
+            public (int X, int Y) Move(int X, int Y)
+            {
+                return (X - 1, Y + 1);
+            }
+        }
+
+        public class NorthEast : IInstruction
+        {
+            public (int X, int Y) Move(int X, int Y)
+            {
+                return (X + 1, Y + 1);
+            }
+        }
+    }
+}
