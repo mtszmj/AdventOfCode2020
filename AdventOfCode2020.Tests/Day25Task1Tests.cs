8c33207fb0158653d7d2642a21d22c0b31b126f0
Mateusz <mateuszmaj@o2.pl>
2021-01-24 15:09:14 +0100

    Day 25 Task 1

---
+++ AdventOfCode2020.Tests/Day25Task1Tests.cs
@@ -0,0 +1,66 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AdventOfCode2020.Day24Task1;
+
+namespace AdventOfCode2020.Tests
+{
+    [TestFixture]
+    public class Day25Task1Tests
+    {
+        Day25Task1 Solver() => new Day25Task1();
+
+        [Test]
+        public void solves_input()
+        {
+            var cardPk = 8987316;
+            var doorPk = 14681524;
+
+            var solver = Solver();
+
+            var result = solver.Solve(cardPk, doorPk);
+
+            result.Should().Be(15217943);
+        }
+
+        [Test]
+        public void solves_example()
+        {
+            var cardPk = 5764801;
+            var doorPk = 17807724;
+
+            var solver = Solver();
+
+            var result = solver.Solve(cardPk, doorPk);
+
+            result.Should().Be(14897079);
+        }
+
+        [Test]
+        public void finds_card_loop_size()
+        {
+            var cardPk = 5764801;
+            var solver = Solver();
+
+            var result = solver.FindCardLoopSize(cardPk);
+
+            result.Should().Be(8);
+        }
+
+        [Test]
+        public void finds_door_loop_size()
+        {
+            var doorPk = 17807724;
+            var solver = Solver();
+
+            var result = solver.FindDoorLoopSize(doorPk);
+
+            result.Should().Be(11);
+        }
+    }
+}
---
+++ AdventOfCode2020/Day25Task1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    public class Day25Task1
+    {
+
+        public long Solve(long cardPk, long doorPk)
+        {
+            var cardLoopSize = FindCardLoopSize(cardPk);
+            var doorLoopSize = FindDoorLoopSize(doorPk);
+
+            var result = 1L;
+            for (var i = 0; i < doorLoopSize; i++)
+                result = Transform(result, cardPk);
+
+            return result;
+        }
+
+        public long FindCardLoopSize(long cardPk)
+        {
+            var cardLoopSize = 0L;
+            var cardResult = 1L;
+            while (cardResult != cardPk)
+            {
+                cardResult = Transform(cardResult, 7);
+                cardLoopSize++;
+            }
+
+            return cardLoopSize;
+        }
+
+        public long FindDoorLoopSize(long doorPk)
+        {
+            var cardLoopSize = 0L;
+            var cardResult = 1L;
+            while (cardResult != doorPk)
+            {
+                cardResult = Transform(cardResult, 7);
+                cardLoopSize++;
+            }
+
+            return cardLoopSize;
+        }
+
+        public long Transform(long current, long subject)
+        {
+            return (current * subject) % 20201227;
+        }
+    }
+}
